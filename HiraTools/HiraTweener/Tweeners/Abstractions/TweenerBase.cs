108cedfef06dafc873384b550715e257033f3a4f
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-08-14 06:55:26 +0530

    Fixed a bug that made the component tween even when the GameObject was set inactive.

--- HiraTools/HiraTweener/Tweeners/Abstractions/TweenerBase.cs
+++ HiraTools/HiraTweener/Tweeners/Abstractions/TweenerBase.cs
@@ -12,39 +12,54 @@ namespace Hiralal.Tweeners
         protected abstract ScriptableObjectVariableReference<T> StartValue { get; }
         protected abstract ScriptableObjectVariableReference<T> EndValue { get; }
         protected abstract Func<T, T, float, T> InterpolationFunction { get; }
-        
+
         //====================================================================== CORE INTERFACE
-        
-        public sealed override void StartTweenForward() => 
-            Tween(StartValue.Value,
-                EndValue.Value,
-                easing,
-                OnForwardCompletion);
 
-        public sealed override void StartTweenBackward() =>
-            Tween(EndValue.Value,
-                StartValue.Value,
-                invertEasingWhenTweeningBackwards
-                    ? InverseEasing
-                    : easing,
-                OnBackwardCompletion);
+        public sealed override void StartTweenForward()
+        {
+            if (isActiveAndEnabled)
+                Tween(StartValue.Value,
+                    EndValue.Value,
+                    easing,
+                    OnForwardCompletion);
+        }
 
-        public sealed override void Pause() { if(TrackerIsValid) CurrentTracker.Pause(); }
+        public sealed override void StartTweenBackward()
+        {
+            if (isActiveAndEnabled)
+                Tween(EndValue.Value,
+                    StartValue.Value,
+                    invertEasingWhenTweeningBackwards
+                        ? InverseEasing
+                        : easing,
+                    OnBackwardCompletion);
+        }
 
-        public sealed override void Resume() { if(TrackerIsValid && CurrentTracker.IsPaused) CurrentTracker.Resume(); }
+        public sealed override void Pause()
+        {
+            if (TrackerIsValid) CurrentTracker.Pause();
+        }
 
-        public sealed override void Stop() { if (TrackerIsValid) CurrentTracker.Stop(); }
+        public sealed override void Resume()
+        {
+            if (TrackerIsValid && CurrentTracker.IsPaused) CurrentTracker.Resume();
+        }
+
+        public sealed override void Stop()
+        {
+            if (TrackerIsValid) CurrentTracker.Stop();
+        }
 
         public sealed override void SetToStart() => Target.Invoke(StartValue.Value);
 
         public sealed override void SetToEnd() => Target.Invoke(EndValue.Value);
-        
+
         //====================================================================== HELPERS
 
         private void Tween(T startValue, T endValue, HiraTweenEaseType easeType, Action callback)
         {
             Stop();
-            
+
             CurrentTracker = new HiraTween(duration,
                 f => Target.Invoke(InterpolationFunction(startValue, endValue, f)),
                 type, easeType, callback).Start();
