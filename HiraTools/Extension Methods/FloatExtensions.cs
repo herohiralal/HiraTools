06f6fcf1ac311ea1b004b8ab44df1fc2df8455aa
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-08-17 08:29:56 +0530

    Created a couple float extensions for converting to degrees.

---
+++ HiraTools/Extension Methods/FloatExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace UnityEngine
+{
+    public static class FloatExtensions
+    {
+        // TODO: Add this to readme.
+        
+        
+        public static float AsDegrees360(this float input)
+        {
+            input %= 360;
+            if (input < 0) input += 360;
+            return input;
+        }
+
+        public static void ConvertToDegrees360(this ref float input)
+        {
+            input %= 360;
+            if (input < 0) input += 360;
+        }
+    }
+}
