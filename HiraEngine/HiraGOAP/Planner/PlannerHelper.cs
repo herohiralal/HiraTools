73f3efc540267b5d9f727ecb9824b1b811eb5a10
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-09 05:56:18 +0530

    Done with a planning algorithm.

---
+++ HiraEngine/HiraGOAP/Planner/PlannerHelper.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Hiralal.Blackboard;
+using UnityEngine;
+
+namespace Hiralal.GOAP.Planner
+{
+    internal static class PlannerHelper
+    {
+        internal static IReadOnlyList<HiraBlackboardValue> ApplyAction(this HiraBlackboardValueSet state, IHiraAction action)
+        {
+            var effects = action.TransitionComponent.Effects;
+            var count = effects.Count;
+            var undoBuffer = new HiraBlackboardValue[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                var effect = effects[i];
+                switch (effect)
+                {
+                    case HiraBlackboardValue<bool> booleanValue:
+                        var booleanTypeSpecificIndex = booleanValue.TypeSpecificIndex;
+                        undoBuffer[i] = new HiraBlackboardValue<bool>(booleanTypeSpecificIndex,
+                            state.booleans[booleanTypeSpecificIndex]);
+                        state.booleans[booleanTypeSpecificIndex] = booleanValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<float> floatValue:
+                        var floatTypeSpecificIndex = floatValue.TypeSpecificIndex;
+                        undoBuffer[i] = new HiraBlackboardValue<float>(floatTypeSpecificIndex,
+                            state.floats[floatTypeSpecificIndex]);
+                        state.floats[floatTypeSpecificIndex] = floatValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<int> intValue:
+                        var intTypeSpecificIndex = intValue.TypeSpecificIndex;
+                        undoBuffer[i] = new HiraBlackboardValue<int>(intTypeSpecificIndex,
+                            state.integers[intTypeSpecificIndex]);
+                        state.integers[intTypeSpecificIndex] = intValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<string> stringValue:
+                        var stringTypeSpecificIndex = stringValue.TypeSpecificIndex;
+                        undoBuffer[i] = new HiraBlackboardValue<string>(stringTypeSpecificIndex,
+                            state.strings[stringTypeSpecificIndex]);
+                        state.strings[stringTypeSpecificIndex] = stringValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<Vector3> vectorValue:
+                        var vectorTypeSpecificIndex = vectorValue.TypeSpecificIndex;
+                        undoBuffer[i] = new HiraBlackboardValue<Vector3>(vectorTypeSpecificIndex,
+                            state.vectors[vectorTypeSpecificIndex]);
+                        state.vectors[vectorTypeSpecificIndex] = vectorValue.TargetValue;
+                        break;
+                }
+            }
+
+            return undoBuffer;
+        }
+
+        internal static void Undo(this HiraBlackboardValueSet state, IReadOnlyList<HiraBlackboardValue> undoBuffer)
+        {
+            for (var count = undoBuffer.Count - 1; count > -1; count--)
+            {
+                switch (undoBuffer[count])
+                {
+                    case HiraBlackboardValue<bool> booleanValue:
+                        state.booleans[booleanValue.TypeSpecificIndex] = booleanValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<float> floatValue:
+                        state.floats[floatValue.TypeSpecificIndex] = floatValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<int> intValue:
+                        state.integers[intValue.TypeSpecificIndex] = intValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<string> stringValue:
+                        state.strings[stringValue.TypeSpecificIndex] = stringValue.TargetValue;
+                        break;
+                    case HiraBlackboardValue<Vector3> vectorValue:
+                        state.vectors[vectorValue.TypeSpecificIndex] = vectorValue.TargetValue;
+                        break;
+                }
+            }
+        }
+
+        internal static bool DoesNotContainValue(this HiraBlackboardValueSet valueSet, HiraBlackboardValue value) =>
+            !valueSet.ContainsValue(value);
+    }
+}
---
+++ HiraEngine/HiraGOAP/Planner/PlannerJob.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hiralal.Blackboard;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace Hiralal.GOAP.Planner
+{
+    public readonly struct PlannerJob<T> where T : IHiraAction
+    {
+        public PlannerJob(float maxFScore, HiraBlackboardValueSet state, IReadOnlyList<HiraBlackboardValue> target,
+            IEnumerable<T> actions, Action<List<T>> planSetter)
+        {
+            this.maxFScore = maxFScore;
+            this.state = state;
+            this.target = target;
+            this.actions = actions;
+            plan = new List<T>();
+            this.planSetter = planSetter;
+        }
+
+        private readonly float maxFScore;
+        private readonly HiraBlackboardValueSet state;
+        private readonly IReadOnlyList<HiraBlackboardValue> target;
+        private readonly IEnumerable<T> actions;
+        private readonly List<T> plan;
+        private readonly Action<List<T>> planSetter;
+
+        public void GeneratePlan()
+        {
+            float threshold = Heuristic;
+            while (true)
+            {
+                var score = PerformHeuristicEstimatedSearch(0, threshold);
+                if (!score.HasValue)
+                {
+                    planSetter(plan);
+                    return;
+                }
+                if (score.Value > maxFScore)
+                {
+                    planSetter(null);
+                    return;
+                }
+                threshold = score.Value;
+            }
+        }
+
+        private float? PerformHeuristicEstimatedSearch(float cost, float threshold)
+        {
+            // Heuristic is the number of values still remaining
+            var heuristic = Heuristic;
+
+            // Check if the estimated cost is larger than threshold
+            var fScore = cost + heuristic;
+            if (fScore > threshold) return fScore;
+
+            // Goal reached
+            if (heuristic == 0) return null;
+
+            var min = float.MaxValue;
+            foreach (var action in actions)
+            {
+                // ignore invalid actions
+                if (!action.TransitionComponent.ArePreConditionsSatisfied(state)) continue;
+
+                // Mutate the state
+                var undoBuffer = state.ApplyAction(action);
+
+                // Calculate new cost
+                var actionCost = action.Cost;
+                cost += actionCost;
+
+                // check if the goal state was reached
+                var score = PerformHeuristicEstimatedSearch(cost, threshold);
+
+                // Undo cost mutation
+                cost -= actionCost;
+
+                // Undo state mutation
+                state.Undo(undoBuffer);
+
+                // If the goal state was reached
+                if (!score.HasValue)
+                {
+                    plan.Add(action);
+                    return null;
+                }
+
+                // If a goal state wasn't reached & the new fScore is smaller than before
+                if (score.Value < min) min = score.Value;
+            }
+
+            return min;
+        }
+
+        private int Heuristic => target.Count(state.DoesNotContainValue);
+    }
+}
