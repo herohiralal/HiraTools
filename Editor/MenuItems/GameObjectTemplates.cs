04f37e70f62bc4aca8b2aed174ac3939c2a231b7
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-09 08:59:26 +0530

    Successfully moved HiraEngine to Unity's Package Manager system.

---
+++ Editor/MenuItems/GameObjectTemplates.cs
@@ -0,0 +1,20 @@
+﻿﻿using Hiralal.HiraWorlds;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hiralal.MenuItems
+{
+    internal static class GameObjectTemplates
+    {
+        [MenuItem("GameObject/Hiralal/HiraWorld Volume", false, 10)]
+        private static void CreateCustomGameObject(MenuCommand menuCommand)
+        {
+            var go = new GameObject("HiraWorld Volume");
+            go.AddComponent<BoxCollider>().isTrigger = true;
+            go.AddComponent<HiraWorldVolume>();
+            
+            if (menuCommand.context is GameObject gameObject) 
+                go.transform.SetParent(gameObject.transform);
+        }
+    }
+}
---
+++ Editor/ScriptableObject Variables/GameEventEditor.cs
@@ -0,0 +1,18 @@
+﻿﻿using UnityEngine;
+
+namespace UnityEditor
+{
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            GUI.enabled = Application.isPlaying;
+            
+            if (GUILayout.Button("Test")) 
+                ((GameEvent) target).Raise();
+
+            GUI.enabled = true;
+        }
+    }
+}
