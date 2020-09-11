51dfb36257e28494141e10857582c26d91b031b1
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-11 08:44:53 +0530

    Initial HiraCreatureAction.cs commit.

---
+++ Runtime/HiraCreatures/AI Controller/HiraCreatureAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Hiralal.Blackboard;
+using Hiralal.GOAP.Actions;
+using Hiralal.GOAP.Transitions;
+
+namespace UnityEngine
+{
+    public abstract class HiraCreatureAction : IHiraAction
+    {
+        protected HiraCreatureAction(HiraWorldStateTransition transition) => Transition = transition;
+        
+        protected readonly HiraWorldStateTransition Transition = null;
+        public bool ArePreConditionsSatisfied(HiraBlackboardValueSet valueSet) => Transition.ArePreConditionsSatisfied(valueSet);
+        public IReadOnlyList<HiraBlackboardValue> Effects => Transition.Effects;
+        public abstract float Cost { get; }
+    }
+}
