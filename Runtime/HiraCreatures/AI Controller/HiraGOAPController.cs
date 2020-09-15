93cc1c9bfc41ec5649797f4dfc1c007ee12db232
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-15 05:44:34 +0530

    Initial commit for the basic framework of running an action on a HiraGOAPController.

--- Runtime/HiraCreatures/AI Controller/HiraGOAPController.cs
+++ Runtime/HiraCreatures/AI Controller/HiraGOAPController.cs
@@ -1,112 +1,102 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using Hiralal.GOAP.Actions;
 using Hiralal.GOAP.Goals;
-using Hiralal.GOAP.Planner;
-using Hiralal.GOAP.Transitions;
-using Hiralal.Utilities;
 
 namespace UnityEngine
 {
     [RequireComponent(typeof(HiraBlackboard))]
     public class HiraGOAPController : HiraController
     {
-        [Space] [Header("Required Components")]
-        [SerializeField] private HiraBlackboard blackboard = null;
-        
-        [Space] [Header("Behaviour Customization")]
-        [SerializeField] private GoalSet goals = null;
-        private List<HiraCreatureAction> actions = null;
+        [Space] [Header("Required Components")] [SerializeField]
+        private HiraBlackboard blackboard = null;
 
-        [Space] [Header("Planner Settings")] 
-        [SerializeField] private BoolReference multiThreaded = null;
-        [SerializeField] private FloatReference maxFScore = null;
-        
-        private readonly ThreadSafeObject<bool> _plannerActive = new ThreadSafeObject<bool>(false);
-        private readonly ThreadSafeObject<bool> _planRequested = new ThreadSafeObject<bool>(false);
-        private readonly ThreadSafeObject<Stack<HiraCreatureAction>> _queuedPlan = new ThreadSafeObject<Stack<HiraCreatureAction>>();
-        
-        
-        private HiraWorldStateTransition _currentGoal = null;
-        private Planner<HiraCreatureAction> _planner = null;
+        [SerializeField] private HiraPlanner planner = null;
 
-        private CancellationTokenSource _cts = new CancellationTokenSource();
+        [Space] [Header("Behaviour Customization")] [SerializeField]
+        private GoalSet goals = null;
+
+        private Stack<HiraCreatureAction> _plan = new Stack<HiraCreatureAction>();
+        private HiraCreatureAction _currentAction = null;
 
         public override void Possess(HiraCreature creature)
         {
             base.Possess(creature);
 
+            planner.RequestPlan();
+
             blackboard.OnBlackboardUpdate += RecalculateGoal;
-            _planner = new Planner<HiraCreatureAction>(blackboard, PlannerCallback);
         }
 
         public override void Dispossess()
         {
-            _cts.Cancel();
-            _planner = null;
             blackboard.OnBlackboardUpdate -= RecalculateGoal;
-            
+
             base.Dispossess();
         }
 
-        #region Planning
-
-        private void RequestPlan()
-        {
-            if (_planRequested.Value) return;
-            _planRequested.Value = true;
-            StartCoroutine(RequestPlanEnumerator());
-        }
-
-        private IEnumerator RequestPlanEnumerator()
-        {
-            while (_plannerActive.Value) yield return null;
-            _plannerActive.Value = true;
-            
-            if(_cts.IsCancellationRequested) _cts = new CancellationTokenSource();
-            
-            _planner.Initialize(maxFScore, _currentGoal, actions);
-            if (multiThreaded) ThreadPool.QueueUserWorkItem(_planner.GeneratePlan, _cts.Token);
-            else _planner.GeneratePlan(CancellationToken.None);
-        }
-        
-        private void PlannerCallback(PlannerResult result, Stack<HiraCreatureAction> newPlan)
-        {
-            switch (result)
-            {
-                case PlannerResult.None:
-                    HiraLogger.LogErrorFormat(this, $"Planner returned an unhandled output.");
-                    break;
-                case PlannerResult.Success:
-                    _queuedPlan.Value = newPlan;
-                    break;
-                case PlannerResult.FScoreOverflow:
-                    HiraLogger.LogWarning($"FScore overflow.", this);
-                    break;
-                case PlannerResult.Cancelled:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(result), result, null);
-            }
-            _planRequested.Value = false;
-            _plannerActive.Value = false;
-        }
-        
-        #endregion
-        
         #region Goal Calculation
 
         private void RecalculateGoal()
         {
             var goal = goals.FirstOrDefault(g => g.ArePreConditionsSatisfied(blackboard.ValueSet));
-            if (goal == _currentGoal) return;
+            if (goal == planner.CurrentGoal) return;
 
-            _currentGoal = goal;
-            RequestPlan();
+            planner.CurrentGoal = goal;
         }
-        
+
         #endregion
+
+        private void Update()
+        {
+            if (_plan == null)
+            {
+                if (planner.Plan.Value != null)
+                {
+                    _plan = planner.Plan.Value;
+                    planner.Plan.Value = null;
+                }
+                else return;
+            }
+
+            if (_currentAction == null)
+            {
+                if (_plan.Count > 0) SetNextActionAsActive();
+                else return;
+            }
+
+            _currentAction.OnActionExecute();
+            
+            var status = _currentAction.Status;
+            switch (status)
+            {
+                case HiraActionStatus.Running:
+                    break;
+                case HiraActionStatus.Failed:
+                    RequestPlan();
+                    break;
+                case HiraActionStatus.Succeeded:
+                    if (_plan.Count == 0) RequestPlan();
+                    else SetNextActionAsActive();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void SetNextActionAsActive()
+        {
+            _currentAction = _plan.Pop();
+            _currentAction.TargetCreature = ControlledCreature;
+            _currentAction.OnActionStart();
+        }
+
+        private void RequestPlan()
+        {
+            _plan = null;
+            _currentAction = null;
+            planner.RequestPlan(true);
+        }
     }
 }
