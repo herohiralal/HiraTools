187194696239f1834379947be2b90d32c02ff96d
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-15 08:15:40 +0530

    Initial commit for HiraCreatureGoToAction.

--- Runtime/HiraCreatures/AI Controller/HiraCreatureAction.cs
+++ Runtime/HiraCreatures/AI Controller/HiraCreatureAction.cs
@@ -7,12 +7,16 @@ namespace UnityEngine
 {
     public abstract class HiraCreatureAction : IHiraAction
     {
+        protected HiraCreatureAction()
+        {
+        }
+
         protected HiraCreatureAction(HiraWorldStateTransition transition) => Transition = transition;
         
         protected readonly HiraWorldStateTransition Transition = null;
         public abstract bool IsApplicableTo(HiraCreature creature);
-        public bool ArePreConditionsSatisfied(HiraBlackboardValueSet valueSet) => Transition.ArePreConditionsSatisfied(valueSet);
-        public IReadOnlyList<HiraBlackboardValue> Effects => Transition.Effects;
+        public virtual bool ArePreConditionsSatisfied(HiraBlackboardValueSet valueSet) => Transition.ArePreConditionsSatisfied(valueSet);
+        public virtual IReadOnlyList<HiraBlackboardValue> Effects => Transition.Effects;
         public abstract float Cost { get; }
         public virtual void BuildPrePlanCache()
         {
@@ -23,7 +27,7 @@ namespace UnityEngine
         protected void MarkCompleted() => Status = HiraActionStatus.Succeeded;
         protected void MarkFailed() => Status = HiraActionStatus.Failed;
         
-        public abstract HiraCreature TargetCreature { protected get; set; }
+        public abstract HiraCreature TargetCreature { set; }
         public virtual void OnActionStart()
         {
             Status = HiraActionStatus.Running;
---
+++ Runtime/HiraCreatures/Creature Components/Movement/HiraCreatureGoToAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Hiralal.Blackboard;
+using UnityEngine;
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace HiraCreatures.Components.Movement
+{
+    public class HiraCreatureGoToAction : HiraCreatureAction
+    {
+        public HiraCreatureGoToAction(Vector3 targetPosition, uint typeSpecificIndex, int positionID)
+        {
+            _targetPosition = targetPosition;
+            _effects = new HiraBlackboardValue[] { new HiraBlackboardValue<int>(typeSpecificIndex, positionID)  };
+        }
+ 
+        public override bool IsApplicableTo(HiraCreature creature) =>
+            creature is IComponentOwner<IHiraCreatureMover>;
+
+        private IHiraCreatureMover _creatureMover = null;
+        private readonly Vector3 _targetPosition;
+        private float _cost = float.MaxValue;
+        private readonly HiraBlackboardValue[] _effects = null;
+
+        public override void BuildPrePlanCache()
+        {
+            _cost = Vector3.Distance(_creatureMover.Position, _targetPosition);
+        }
+        
+        public override HiraCreature TargetCreature 
+        {
+            set
+            {
+                if (value is IComponentOwner<IHiraCreatureMover> movableCreature)
+                    _creatureMover = movableCreature.Component;
+            }
+        }
+
+        public override float Cost => _cost;
+
+        public override IReadOnlyList<HiraBlackboardValue> Effects => _effects;
+    }
+}
