382c4ba64f66f4477ca821a8fcb58c6a1c2e4e9a
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-15 10:08:31 +0530

    Moved the effect of a GoTo action to a common reference.

--- Runtime/HiraCreatures/Creature Components/Movement/HiraCreatureGoToAction.cs
+++ Runtime/HiraCreatures/Creature Components/Movement/HiraCreatureGoToAction.cs
@@ -7,10 +7,10 @@ namespace HiraCreatures.Components.Movement
 {
     public class HiraCreatureGoToAction : HiraCreatureAction
     {
-        public HiraCreatureGoToAction(Vector3 targetPosition, uint typeSpecificIndex, int positionID)
+        public HiraCreatureGoToAction(Vector3 targetPosition, IReadOnlyList<HiraBlackboardValue> effects)
         {
             _targetPosition = targetPosition;
-            _effects = new HiraBlackboardValue[] { new HiraBlackboardValue<int>(typeSpecificIndex, positionID)  };
+            Effects = effects;
         }
  
         public override bool IsApplicableTo(HiraCreature creature) =>
@@ -19,7 +19,6 @@ namespace HiraCreatures.Components.Movement
         private IHiraCreatureMover _creatureMover = null;
         private readonly Vector3 _targetPosition;
         private float _cost = float.MaxValue;
-        private readonly HiraBlackboardValue[] _effects = null;
 
         public override void BuildPrePlanCache()
         {
@@ -37,6 +36,6 @@ namespace HiraCreatures.Components.Movement
 
         public override float Cost => _cost;
 
-        public override IReadOnlyList<HiraBlackboardValue> Effects => _effects;
+        public override IReadOnlyList<HiraBlackboardValue> Effects { get; }
     }
 }
