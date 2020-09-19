f02c7e8fabb0c870c3042842f2597b408d942b22
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-20 00:30:24 +0530

    Added accessors for DoesNotSatisfy on datasets/values.

--- Runtime/HiraCreatures/Controller Components/Blackboard/Helpers/BlackboardDataValueChecker.cs
+++ Runtime/HiraCreatures/Controller Components/Blackboard/Helpers/BlackboardDataValueChecker.cs
@@ -33,5 +33,33 @@ namespace UnityEngine
 
         public static bool IsSatisfiedBy(this IEnumerable<IBlackboardValue> values,IBlackboardValueAccessor accessor) => 
             values.All(accessor.DataSet.Satisfies);
+        
+        // .DoesNotSatisfy(value)
+        public static bool DoesNotSatisfy(this IReadOnlyBlackboardDataSet dataSet, IBlackboardValue value) =>
+            !value.IsSatisfiedBy(dataSet);
+
+        public static bool DoesNotSatisfy(this IBlackboardValueAccessor accessor, IBlackboardValue value) =>
+            !value.IsSatisfiedBy(accessor.DataSet);
+        
+        // .DoesNotSatisfy(values)
+        public static bool DoesNotSatisfy(this IReadOnlyBlackboardDataSet dataSet, IEnumerable<IBlackboardValue> values) => 
+            !values.All(dataSet.Satisfies);
+
+        public static bool DoesNotSatisfy(this IBlackboardValueAccessor accessor, IEnumerable<IBlackboardValue> values) => 
+            !values.All(accessor.DataSet.Satisfies);
+        
+        // value.IsNotSatisfiedBy
+        public static bool IsNotSatisfiedBy(this IBlackboardValue value, IReadOnlyBlackboardDataSet dataSet) =>
+            !value.IsSatisfiedBy(dataSet);
+        
+        public static bool IsNotSatisfiedBy(this IBlackboardValue value, IBlackboardValueAccessor accessor) => 
+            !value.IsSatisfiedBy(accessor.DataSet);
+
+        // values.IsNotSatisfiedBy
+        public static bool IsNotSatisfiedBy(this IEnumerable<IBlackboardValue> values,IReadOnlyBlackboardDataSet dataSet) => 
+            !values.All(dataSet.Satisfies);
+
+        public static bool IsNotSatisfiedBy(this IEnumerable<IBlackboardValue> values,IBlackboardValueAccessor accessor) => 
+            !values.All(accessor.DataSet.Satisfies);
     }
 }
