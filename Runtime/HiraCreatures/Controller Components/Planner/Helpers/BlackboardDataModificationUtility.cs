6d8a3c15f805f6e4e19bdb2cc84f1c752cb925dc
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-23 23:38:24 +0530

    Created a utility to apply blackboard modification/hybrid values to a dataset easily.

---
+++ Runtime/HiraCreatures/Controller Components/Planner/Helpers/BlackboardDataModificationUtility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using HiraEngine.Components.Planner.Internal;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+
+namespace UnityEngine
+{
+    public static class BlackboardDataModificationUtility
+    {
+        // Apply(value)
+        public static IBlackboardModification Apply(this IReadWriteBlackboardDataSet dataSet,
+            IBlackboardModification modification) =>
+            modification.ApplyTo(dataSet);
+
+        // Values.Apply
+        public static UndoStack ApplyTo(
+            this IReadOnlyList<IBlackboardModification> modifications, IReadWriteBlackboardDataSet dataSet)
+        {
+            var count = modifications.Count;
+            var undoBuffer = new IBlackboardModification[count];
+            for (var i = 0; i < count; i++) undoBuffer[i] = modifications[i].ApplyTo(dataSet);
+            return new UndoStack(undoBuffer);
+        }
+
+        // Apply(values)
+        public static UndoStack Apply(this IReadWriteBlackboardDataSet dataSet,
+            IReadOnlyList<IBlackboardModification> modifications) =>
+            modifications.ApplyTo(dataSet);
+
+        public static void Apply(this IReadWriteBlackboardDataSet dataSet,
+            UndoStack undoStack) =>
+            undoStack.ApplyTo(dataSet);
+    }
+}
---
+++ Runtime/HiraCreatures/Controller Components/Planner/Internal/UndoStack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace HiraEngine.Components.Planner.Internal
+{
+    public readonly struct UndoStack
+    {
+        public UndoStack(IReadOnlyList<IBlackboardModification> stack) => _stack = stack;
+        private readonly IReadOnlyList<IBlackboardModification> _stack;
+
+        public void ApplyTo(IReadWriteBlackboardDataSet dataSet)
+        {
+            for (var count = _stack.Count - 1; count  > -1; count--)
+            {
+                _stack[count].ApplyTo(dataSet);
+            }
+        }
+    }
+}
