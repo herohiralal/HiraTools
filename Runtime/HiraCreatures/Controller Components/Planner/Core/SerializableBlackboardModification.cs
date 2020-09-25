56d720254e96ebfd2a863d6b760ec04db5c4221e
Rohan Jadav <ro1.darkstar7@gmail.com>
2020-09-25 14:46:06 +0530

    Moved Serializable BlackboardValues to their individual files.

---
+++ Runtime/HiraCreatures/Controller Components/Planner/Core/SerializableBlackboardModification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using HiraEngine.Components.Planner.Internal;
+
+namespace UnityEngine
+{
+    [Serializable]
+    public class SerializableBlackboardModification : SerializableBlackboardValue
+    {
+        public IBlackboardModification Modification => PlannerTypes.GetModification(typeString, this);
+    }
+}
--- Runtime/HiraCreatures/Controller Components/Planner/Core/SerializableBlackboardQuery.cs
+++ Runtime/HiraCreatures/Controller Components/Planner/Core/SerializableBlackboardQuery.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using HiraEngine.Components.Planner;
 using HiraEngine.Components.Planner.Internal;
 
 namespace UnityEngine
@@ -8,17 +7,5 @@ namespace UnityEngine
     public class SerializableBlackboardQuery : SerializableBlackboardValue
     {
         public IBlackboardQuery Query => PlannerTypes.GetQuery(typeString, this);
-    }
-
-    [Serializable]
-    public class SerializableBlackboardModification : SerializableBlackboardValue
-    {
-        public IBlackboardModification Modification => PlannerTypes.GetModification(typeString, this);
-    }
-
-    [Serializable]
-    public class SerializableBlackboardHybridValue : SerializableBlackboardValue
-    {
-        public IBlackboardHybridValue HybridValue => PlannerTypes.GetHybridValue(typeString, this);
     }
 }
