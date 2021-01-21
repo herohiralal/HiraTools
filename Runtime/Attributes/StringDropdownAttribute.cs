41def87910c9f4408713782735f9ff97dbf1f4cf
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-21 14:55:34 +0530

    Added StringDropdownAttribute

---
+++ Runtime/Attributes/StringDropdownAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityEngine
+{
+    public class StringDropdownAttribute : PropertyAttribute
+    {
+        public StringDropdownAttribute(bool editable = true, params string[] dropdownValues) =>
+            (Editable, DropdownValues) = (editable, dropdownValues);
+
+        public readonly bool Editable;
+        public readonly string[] DropdownValues;
+    }
+}
