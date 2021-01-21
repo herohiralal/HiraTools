d509030609fd990ff557eee7764af9929dc70fb1
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-21 17:05:48 +0530

    Added HiraScriptCreator.

---
+++ Editor/HiraScriptCreator/DoCreateHiraScript.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+namespace HiraEditor
+{
+    public class DoCreateHiraScript : EndNameEditAction
+    {
+        [MenuItem("Assets/Create/CreateScript")]
+        private static void Create()
+        {
+            var scriptCreator = (HiraScriptCreator) Selection.activeObject;
+            var icon = (Texture2D) EditorGUIUtility.IconContent("cs Script Icon").image;
+            var sourcePath = AssetDatabase.GetAssetPath(scriptCreator);
+            var sourceDir = Path.GetDirectoryName(sourcePath)?.Replace('\\', '/');
+            var classPath = $"{sourceDir}/{scriptCreator.FileName}.cs";
+            var creator = CreateInstance<DoCreateHiraScript>();
+
+            if (File.Exists(classPath))
+                ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, creator,
+                    classPath, icon, scriptCreator.FileData);
+            else
+            {
+                creator.Action(0, classPath, scriptCreator.FileData);
+                creator.CleanUp();
+            }
+        }
+
+        [MenuItem("Assets/Create/CreateScript", true)]
+        private static bool ValidateCreation()
+        {
+            var activeObject = Selection.activeObject;
+            return activeObject != null && activeObject is HiraScriptCreator;
+        }
+
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+            File.WriteAllText(Path.GetFullPath(pathName), FixLineEndings(resourceFile), new UTF8Encoding(true));
+            
+            AssetDatabase.ImportAsset(pathName);
+            
+            var o = AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
+            ProjectWindowUtil.ShowCreatedAsset(o);
+        }
+
+        private static string FixLineEndings(string content)
+        {
+            const string windowsLineEndings = "\r\n";
+            const string unixLineEndings = "\n";
+
+            var preferredLineEndings = EditorSettings.lineEndingsForNewScripts switch
+            {
+                LineEndingsMode.OSNative => Application.platform == RuntimePlatform.WindowsEditor
+                    ? windowsLineEndings
+                    : unixLineEndings,
+                LineEndingsMode.Unix => unixLineEndings,
+                LineEndingsMode.Windows => windowsLineEndings,
+                _ => unixLineEndings
+            };
+
+            content = Regex.Replace(content, @"\r\n?|\n", preferredLineEndings);
+
+            return content;
+        }
+    }
+}
---
+++ Runtime/HiraScriptCreator/HiraScriptCreator.cs
@@ -0,0 +1,60 @@
+﻿namespace UnityEngine
+{
+    public abstract class HiraScriptCreator : ScriptableObject
+    {
+        public abstract string FileName { get; }
+        public abstract string FileData { get; }
+    }
+}
+
+// [SerializeField] private KeyData[] keyData = null;
+//
+// [Serializable]
+// private struct KeyData
+// {
+//     public string keyName;
+//
+//     [StringDropdown(false, "bool", "float", "int", "string")]
+//     public string type;
+// }
+//
+// public string GenerateFileData()
+// {
+//     var sb = new StringBuilder(500);
+//     sb
+//         .AppendLine(@"using System;")
+//         .AppendLine(@"")
+//         .AppendLine($"public class {name}Blackboard")
+//         .AppendLine(@"{")
+//         .AppendLine(@"    public event Action OnValueUpdate = delegate { };")
+//         .AppendLine(@"    ");
+//
+//     foreach (var data in keyData)
+//     {
+//         var formattedKeyName = FormatToPrivateField(data.keyName);
+//         sb
+//             .AppendLine($"    private {data.type} {formattedKeyName} = default;")
+//             .AppendLine($"    public {data.type} {data.keyName}")
+//             .AppendLine(@"    {")
+//             .AppendLine($"        get => {formattedKeyName};")
+//             .AppendLine(@"        set")
+//             .AppendLine(@"        {")
+//             .AppendLine(@"            OnValueUpdate.Invoke();")
+//             .AppendLine($"            {formattedKeyName} = value;")
+//             .AppendLine(@"        }")
+//             .AppendLine(@"    }")
+//             .AppendLine(@"    ");
+//     }
+//
+//     return sb.AppendLine(@"}")
+//         .ToString();
+// }
+//
+// private static string FormatToPrivateField(string name)
+// {
+//     if (string.IsNullOrWhiteSpace(name)) return "";
+//
+//     var characters = name.ToCharArray();
+//     characters[0] = char.ToLower(characters[0]);
+//     return "_" + new string(characters);
+// }
