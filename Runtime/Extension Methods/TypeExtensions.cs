af99e0ac3d93a1144d4d556ea4ca18082061190e
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-24 12:08:15 +0530

    Added extension methods to get ancestor types.

--- Runtime/Extension Methods/TypeExtensions.cs
+++ Runtime/Extension Methods/TypeExtensions.cs
@@ -5,26 +5,40 @@ namespace System
 {
     public static class TypeExtensions
     {
+        public static IEnumerable<string> GetNamesOfClasses(this IEnumerable<Type> types) =>
+            types.Select(t => t.Name);
+        
         public static IEnumerable<Type> GetSubclasses(this Type baseClass) =>
             AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(assembly => assembly.GetTypes()
                     .Where(t => baseClass.IsAssignableFrom(t) && !t.IsAbstract));
 
         public static IEnumerable<Type> GetSubclasses<TBase>() =>
-            AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(assembly => assembly.GetTypes()
-                    .Where(t => (typeof(TBase)).IsAssignableFrom(t) && !t.IsAbstract));
-
-        private static IEnumerable<string> GetNamesOfSubclasses(this IEnumerable<Type> types) =>
-            types.Select(t => t.Name);
+            typeof(TBase).GetSubclasses();
 
         public static IEnumerable<string> GetNamesOfSubclasses(this Type baseClass) =>
             GetSubclasses(baseClass)
-                .GetNamesOfSubclasses();
+                .GetNamesOfClasses();
 
         public static IEnumerable<string> GetNamesOfSubclasses<TBase>() =>
             GetSubclasses<TBase>()
-                .GetNamesOfSubclasses();
+                .GetNamesOfClasses();
+
+        public static IEnumerable<Type> GetAncestorTypes(this Type baseClass) =>
+            AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(t => t.IsAssignableFrom(baseClass) && t != typeof(object));
+
+        public static IEnumerable<Type> GetAncestorTypes<TBase>() =>
+            typeof(TBase).GetAncestorTypes();
+
+        public static IEnumerable<string> GetNamesOfAncestorTypes(this Type baseClass) =>
+            GetAncestorTypes(baseClass)
+                .GetNamesOfClasses();
+
+        public static IEnumerable<string> GetNamesOfAncestorTypes<TBase>() =>
+            GetAncestorTypes<TBase>()
+                .GetNamesOfClasses();
 
         public static string GetReflectionName(this Type type) =>
             $"{type.FullName}, {type.Assembly.FullName}";
