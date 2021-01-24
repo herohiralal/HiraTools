e83109a32152a12a654e2ac53a377cc9b8f01053
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-24 17:03:12 +0530

    Added extension methods to get full hierarchies and parent classes of a certain type.

--- Runtime/Extension Methods/TypeExtensions.cs
+++ Runtime/Extension Methods/TypeExtensions.cs
@@ -3,44 +3,32 @@ using System.Linq;
 
 namespace System
 {
-    public static class TypeExtensions
-    {
-        public static IEnumerable<string> GetNamesOfClasses(this IEnumerable<Type> types) =>
-            types.Select(t => t.Name);
-        
-        public static IEnumerable<Type> GetSubclasses(this Type baseClass) =>
-            AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(assembly => assembly.GetTypes()
-                    .Where(t => baseClass.IsAssignableFrom(t) && !t.IsAbstract));
+	public static class TypeExtensions
+	{
+		public static IEnumerable<Type> AllTypes =>
+			AppDomain.CurrentDomain.GetAssemblies()
+				.SelectMany(assembly => assembly.GetTypes());
 
-        public static IEnumerable<Type> GetSubclasses<TBase>() =>
-            typeof(TBase).GetSubclasses();
+		public static IEnumerable<string> GetNamesOfClasses(this IEnumerable<Type> types) =>
+			types.Select(t => t.Name);
 
-        public static IEnumerable<string> GetNamesOfSubclasses(this Type baseClass) =>
-            GetSubclasses(baseClass)
-                .GetNamesOfClasses();
+		public static IEnumerable<Type> GetSubClasses(this Type baseClass, bool allowAbstract = false,
+			bool allowSelf = true) =>
+			AllTypes.Where(t =>
+				baseClass.IsAssignableFrom(t) && (allowAbstract || !t.IsAbstract) && (allowSelf || t != baseClass));
 
-        public static IEnumerable<string> GetNamesOfSubclasses<TBase>() =>
-            GetSubclasses<TBase>()
-                .GetNamesOfClasses();
+		public static IEnumerable<Type> GetBaseTypes(this Type childClass, bool allowAbstract = false,
+			bool allowSelf = true) =>
+			AllTypes.Where(t =>
+				t.IsAssignableFrom(childClass) && t != typeof(object) && (allowAbstract || !t.IsAbstract) &&
+				(allowSelf || t != childClass));
 
-        public static IEnumerable<Type> GetAncestorTypes(this Type baseClass) =>
-            AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(assembly => assembly.GetTypes())
-                .Where(t => t.IsAssignableFrom(baseClass) && t != typeof(object));
+		public static IEnumerable<Type> GetHierarchy(this Type target, bool allowAbstract = true,
+			bool includeSelf = true) =>
+			target.GetBaseTypes(allowAbstract, false)
+				.Concat(target.GetSubClasses(allowAbstract, includeSelf));
 
-        public static IEnumerable<Type> GetAncestorTypes<TBase>() =>
-            typeof(TBase).GetAncestorTypes();
-
-        public static IEnumerable<string> GetNamesOfAncestorTypes(this Type baseClass) =>
-            GetAncestorTypes(baseClass)
-                .GetNamesOfClasses();
-
-        public static IEnumerable<string> GetNamesOfAncestorTypes<TBase>() =>
-            GetAncestorTypes<TBase>()
-                .GetNamesOfClasses();
-
-        public static string GetReflectionName(this Type type) =>
-            $"{type.FullName}, {type.Assembly.FullName}";
-    }
+		public static string GetReflectionName(this Type type) =>
+			$"{type.FullName}, {type.Assembly.FullName}";
+	}
 }
