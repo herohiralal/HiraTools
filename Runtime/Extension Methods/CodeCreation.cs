944157f85ace67aca4ee179bfffa4232344855bf
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-28 05:49:35 +0530

    Added pascal to camel case conversion in code creation.

--- Runtime/Extension Methods/CodeCreation.cs
+++ Runtime/Extension Methods/CodeCreation.cs
@@ -6,4 +6,12 @@ public static class CodeCreation
 	public static string ToCode(this bool b) => b.ToString().ToLowerInvariant();
 	public static string ToCode(this UnityEngine.Vector3 v) => $"new Vector3({v.x}f, {v.y}f, {v.z}f)";
 	public static string ToCode(this string s) => $"\"{s}\"";
+
+	public static string PascalToCamel(this string name, bool underscore = true)
+	{
+		var charArray = name.ToCharArray();
+		charArray[0] = char.ToLowerInvariant(charArray[0]);
+		var s = new string(charArray);
+		return underscore ? $"_{s}" : s;
+	}
 }
