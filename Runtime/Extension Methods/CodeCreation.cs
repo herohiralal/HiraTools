d2206e51846ddb5a6fac18257e0a5fa895582c63
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-28 10:24:50 +0530

    Improved enum to code conversion.

--- Runtime/Extension Methods/CodeCreation.cs
+++ Runtime/Extension Methods/CodeCreation.cs
@@ -1,7 +1,27 @@
 public static class CodeCreation
 {
 	public static string ToCode(this float f) => $"{f}f";
-	public static string ToCode<T>(this T e) where T : System.Enum => $"{typeof(T).FullName}.{e}";
+	public static string ToCode<T>(this T e) where T : System.Enum
+	{
+		var typeString = typeof(T).FullName;
+		var enumString = e.ToString();
+		if (!enumString.Contains(", ")) return $"{typeString}.{enumString}";
+		
+		// flags
+		var values = enumString.Split(new[]{", "}, System.StringSplitOptions.RemoveEmptyEntries);
+		var sb = new System.Text.StringBuilder(250);
+		var valuesLength = values.Length;
+		for (var index = 0; index < valuesLength; index++)
+		{
+			sb.Append($"{typeString}.{values[index]}");
+			if (index != valuesLength - 1)
+				sb.Append(" | ");
+		}
+
+		return sb.ToString();
+
+	}
+
 	public static string ToCode(this int i) => i.ToString();
 	public static string ToCode(this bool b) => b.ToString().ToLowerInvariant();
 	public static string ToCode(this UnityEngine.Vector3 v) => $"new Vector3({v.x}f, {v.y}f, {v.z}f)";
