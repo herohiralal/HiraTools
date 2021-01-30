6d5621274eba648e555c64f50243f636dbfe0067
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-30 15:09:46 +0530

    Improved StringConcatenationWithConnector.

--- Runtime/Extension Methods/StringExtensions.cs
+++ Runtime/Extension Methods/StringExtensions.cs
@@ -7,15 +7,15 @@ namespace UnityEngine
         public static int GetAnimatorHash(this string current) => Animator.StringToHash(current);
         public static int GetShaderHash(this string current) => Shader.PropertyToID(current);
 
-        public static string ConcatenateStringsWith<T>(this T[] input, Func<T, string> preprocessor, string connector)
+        public static string ConcatenateStringsWith<T>(this T[] input, Func<T, string> preprocessor, string connector, string prefix = "", string suffix = "")
         {
-            var s = "";
+            var s = prefix;
             
             var inputLength = input.Length;
             for (var i = 0; i < inputLength; i++)
             {
                 s += preprocessor.Invoke(input[i]);
-                if (i != inputLength - 1) s += connector;
+                s += i == inputLength - 1 ? suffix : connector;
             }
 
             return s;
