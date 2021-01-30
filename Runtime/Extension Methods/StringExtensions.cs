5bff67ade3514668c5bbf0b852f3463ca789e2b3
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-30 13:27:02 +0530

    Incorporated goal target heuristic calculation to blackboard code generation.

--- Runtime/Extension Methods/StringExtensions.cs
+++ Runtime/Extension Methods/StringExtensions.cs
@@ -1,8 +1,24 @@
-﻿namespace UnityEngine
+﻿using System;
+
+namespace UnityEngine
 {
     public static class StringExtensions
     {
         public static int GetAnimatorHash(this string current) => Animator.StringToHash(current);
         public static int GetShaderHash(this string current) => Shader.PropertyToID(current);
+
+        public static string ConcatenateStringsWith<T>(this T[] input, Func<T, string> preprocessor, string connector)
+        {
+            var s = "";
+            
+            var inputLength = input.Length;
+            for (var i = 0; i < inputLength; i++)
+            {
+                s += preprocessor.Invoke(input[i]);
+                if (i != inputLength - 1) s += connector;
+            }
+
+            return s;
+        }
     }
 }
--- Runtime/HiraScriptCreator/Blackboard/Transitions/BlackboardGoal.cs
+++ Runtime/HiraScriptCreator/Blackboard/Transitions/BlackboardGoal.cs
@@ -1,8 +1,19 @@
 namespace UnityEngine
 {
-	public class BlackboardGoal : ScriptableObject, IBlackboardGoal
+	public class BlackboardGoal : HiraCollection<IIndividualQuery, IIndividualQuery>, IBlackboardGoal
 	{
+#if UNITY_EDITOR && !STRIP_EDITOR_CODE
+#pragma warning disable 414
+		private static readonly string collection1_name = "Requirements";
+		private static readonly string collection2_name = "Targets";
+#pragma warning restore 414
+#endif
+		
 		public string Name => name;
-		public string TargetHeuristicString => "0";
+		public string TargetHeuristicString =>
+			Collection2
+				.ConcatenateStringsWith(
+					q=>$"({q.Condition} ? 0 : {q.Weight})", 
+					" + ");
 	}
 }
