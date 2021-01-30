513797c772b6c93151d1fc4d8dc44b85b728b75b
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-01-30 13:26:29 +0530

    Created boolean comparison query.

---
+++ Runtime/HiraScriptCreator/Blackboard/Queries/BoolComparisonQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace UnityEngine
+{
+    public class BoolComparisonQuery : ScriptableObject, IIndividualQuery
+    {
+        [SerializeField] private float weight = 1;
+        public float Weight => weight;
+        
+        [HiraCollectionDropdown(typeof(BooleanKey))]
+        [SerializeField] protected BlackboardKey key = null;
+
+        [SerializeField] private bool invert = false;
+
+
+        public string Condition => $"({(invert?"!":"")}blackboard->{key.name})";
+    }
+}
