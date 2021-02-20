5f6ab236795172bf7b5493b41ead950a88379988
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-02-20 11:07:08 +0530

    Added: limited debug support in console tester and editor DLL of HiraEngine-Native.

--- Plugins/.Source/HiraEngine-Native/Tester/ConsoleTester.cpp
+++ Plugins/.Source/HiraEngine-Native/Tester/ConsoleTester.cpp
@@ -1,12 +1,37 @@
 ﻿#ifdef _CONSOLE
-
+#include "Debug.h"
 #include <iostream>
 
 #define PRINT( x ) std::cout << x << std::endl;
 
+void __stdcall LogToConsole(const ELogType LogType, const char* ToLog)
+{
+    switch (LogType)
+    {
+        case Error:
+            std::cout << ">> [ERROR]     ";
+            break;
+        case Assert:
+            std::cout << ">> [ASSERTION] ";
+            break;
+        case Warning:
+            std::cout << ">> [WARNING]   ";
+            break;
+        case Log:
+            std::cout << ">> [MESSAGE]   ";
+            break;
+        case Exception:
+            std::cout << ">> [EXCEPTION] ";
+            break;
+        default: ;
+    }
+    PRINT(ToLog)
+}
+
 int main(int Argc, char* Argv[])
-{    
-    
+{
+    // attach console output
+    Debug::__InternalLogToUnity = LogToConsole;
 }
 
 #endif
