b8cae93fd6ba8fa25c947ee05d2bea3afa2b3079
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-02-22 14:05:32 +0530

    Made Enum Flag operators constexpr instead of inline.

--- Plugins/.Source/HiraEngine-Native/HelperMacros/SyntacticMacros.h
+++ Plugins/.Source/HiraEngine-Native/HelperMacros/SyntacticMacros.h
@@ -110,13 +110,13 @@
     { \
         FOR_EACH(WRITE_ENUM_NAME, __VA_ARGS__) \
     }; \
-    inline E##name operator~(const E##name A) { return static_cast<E##name>(~static_cast<type>(A)); } \
-    inline E##name operator|(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) | static_cast<type>(B)); } \
-    inline E##name operator&(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) & static_cast<type>(B)); } \
-    inline E##name operator^(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) ^ static_cast<type>(B)); } \
-    inline E##name& operator|=(E##name& A, const E##name B) { A = A | B; return A; } \
-    inline E##name& operator&=(E##name& A, const E##name B) { A = A & B; return A; } \
-    inline E##name& operator^=(E##name& A, const E##name B) { A = A ^ B; return A; } \
-    inline bool HasFlag(const E##name A, const E##name B) { return ((A & B) == B); } \
-    inline void AddFlag(E##name& A, const E##name B) { A |= B; } \
-    inline void RemoveFlag(E##name& A, const E##name B) { A &= ~B; }
+    constexpr E##name operator~(const E##name A) { return static_cast<E##name>(~static_cast<type>(A)); } \
+    constexpr E##name operator|(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) | static_cast<type>(B)); } \
+    constexpr E##name operator&(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) & static_cast<type>(B)); } \
+    constexpr E##name operator^(const E##name A, const E##name B) { return static_cast<E##name>(static_cast<type>(A) ^ static_cast<type>(B)); } \
+    constexpr E##name& operator|=(E##name& A, const E##name B) { A = A | B; return A; } \
+    constexpr E##name& operator&=(E##name& A, const E##name B) { A = A & B; return A; } \
+    constexpr E##name& operator^=(E##name& A, const E##name B) { A = A ^ B; return A; } \
+    constexpr bool HasFlag(const E##name A, const E##name B) { return ((A & B) == B); } \
+    constexpr void AddFlag(E##name& A, const E##name B) { A |= B; } \
+    constexpr void RemoveFlag(E##name& A, const E##name B) { A &= ~B; }
