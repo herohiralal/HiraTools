75861e7c167e85dba9d14a0c34956b7995695e82
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-21 20:33:39 +0530

    Added new score calculators.

--- Runtime/HiraComponents/Blackboard/DatabaseFunctions/ScoreCalculators/BooleanEqualsCalculator.cs
+++ Runtime/HiraComponents/Blackboard/DatabaseFunctions/ScoreCalculators/BooleanEqualsCalculator.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 namespace HiraEngine.Components.Blackboard.Internal
 {
-	[Serializable, BurstCompile]
+	[Serializable, BurstCompile, HiraBlackboardScoreCalculator]
 	public class BooleanEqualsCalculator : BooleanEqualsDecorator, IBlackboardScoreCalculator
 	{
 		[SerializeField] private float score = 0f;
 
 		public float Score => score;
 
-		public override string ToString() => $"{base.ToString()} ({score:+0;-$#})";
+		public override string ToString() => $"{base.ToString()} ({score:+0;-#})";
 	}
 }
---
+++ Runtime/HiraComponents/Blackboard/DatabaseFunctions/ScoreCalculators/EnumEqualsCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Burst;
+using UnityEngine;
+
+namespace HiraEngine.Components.Blackboard.Internal
+{
+	[Serializable, BurstCompile, HiraBlackboardScoreCalculator]
+	public class EnumEqualsCalculator : EnumEqualsDecorator, IBlackboardScoreCalculator
+	{
+		[SerializeField] private float score = 0f;
+
+		public float Score => score;
+
+		public override string ToString() => $"{base.ToString()} ({score:+0;-#})";
+	}
+}
