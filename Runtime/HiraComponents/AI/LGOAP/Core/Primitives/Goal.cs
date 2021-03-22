27658c13ec42f26cfc37c0552b9fdf977bd04b11
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 16:33:18 +0530

    Created LGOAP primitives.

---
+++ Runtime/HiraComponents/AI/LGOAP/Core/Primitives/Goal.cs
@@ -0,0 +1,13 @@
+﻿using HiraEngine.Components.Blackboard.Internal;
+using Unity.Burst;
+using UnityEngine;
+
+namespace HiraEngine.Components.AI.LGOAP
+{
+    [BurstCompile]
+    [HiraCollectionCustomizer(1, Title = "Insistence-Calculators", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardScoreCalculatorAttribute)})]
+    [HiraCollectionCustomizer(2, Title = "Targets", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(IBlackboardDecorator)})]
+    public sealed class Goal : HiraCollection<IBlackboardScoreCalculator, IBlackboardDecorator>
+    {
+    }
+}
