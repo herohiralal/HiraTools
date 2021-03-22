d6acf12bd8cd78741382dfce68892660ec106543
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 16:33:18 +0530

    Created LGOAP primitives.

---
+++ Runtime/HiraComponents/AI/LGOAP/Core/Primitives/Action.cs
@@ -0,0 +1,14 @@
+﻿using HiraEngine.Components.Blackboard.Internal;
+using UnityEngine;
+
+namespace HiraEngine.Components.AI.LGOAP
+{
+    [HiraCollectionCustomizer(1, Title = "Preconditions", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardDecoratorAttribute)})]
+    [HiraCollectionCustomizer(2, Title = "Cost-Calculators", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardScoreCalculatorAttribute)})]
+    [HiraCollectionCustomizer(3, Title = "Effects", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardEffectorAttribute)})]
+    [HiraCollectionCustomizer(4, Title = "Task", MaxObjectCount = 1)]
+    [HiraCollectionCustomizer(5, Title = "Services")]
+    public sealed class Action : HiraCollection<IBlackboardDecorator, IBlackboardScoreCalculator, IBlackboardEffector, IExecutableProvider, IServiceProvider>
+    {
+    }
+}
