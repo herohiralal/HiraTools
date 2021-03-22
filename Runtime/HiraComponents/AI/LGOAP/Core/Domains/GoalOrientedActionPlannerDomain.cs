7423dc90ebd4e8f6f3d4798f73c22d8bc980901a
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 17:51:17 +0530

    Created basic framework for domains.

---
+++ Runtime/HiraComponents/AI/LGOAP/Core/Domains/GoalOrientedActionPlannerDomain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using HiraEngine.Components.AI.LGOAP.Internal;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HiraEngine.Components.AI.LGOAP
+{
+    [CreateAssetMenu(menuName = "Hiralal/AI/GOAP Domain", fileName = "New GOAPDomain")]
+    [HiraCollectionCustomizer(1, MaxObjectCount = byte.MaxValue, Title = "Goals")]
+    [HiraCollectionCustomizer(2, MaxObjectCount = byte.MaxValue, Title = "Actions")]
+    public unsafe class GoalOrientedActionPlannerDomain : HiraCollection<Goal, Action>, IInitializable
+    {
+        [NonSerialized] private NativeArray<byte> _domainData = default;
+
+        public void Initialize<T>(ref T initParams)
+        {
+            ushort size = 0;
+            
+            var insistenceCalculators = Collection1.Select(g=>g.Collection1).ToArray();
+            var targets = Collection1.Select(g => g.Collection2).ToArray();
+            var actions = Collection2.Select(a => (a.Collection1, a.Collection2, a.Collection3)).ToArray();
+
+            var insistenceCalculatorsBlockSize = insistenceCalculators.GetInsistenceCalculatorsBlockSize();
+            size += insistenceCalculatorsBlockSize;
+            
+            var layerBlockSize = LayerMemoryBatchHelpers.GetLayerMemorySize(targets, actions);
+            size += layerBlockSize;
+
+            _domainData = new NativeArray<byte>(size, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+            var stream = (byte*) _domainData.GetUnsafePtr();
+
+            var insistenceCalculatorsAllocatedBlockSize = insistenceCalculators.AppendInsistenceCalculatorsBlock(stream);
+            Assert.AreEqual(insistenceCalculatorsBlockSize, insistenceCalculatorsAllocatedBlockSize);
+            stream += insistenceCalculatorsAllocatedBlockSize;
+
+            var layerAllocatedBlockSize = LayerMemoryBatchHelpers.AppendEntireLayerBlock(targets, actions, stream);
+            Assert.AreEqual(layerBlockSize, layerAllocatedBlockSize);
+        }
+
+        public void Shutdown()
+        {
+            _domainData.Dispose();
+        }
+    }
+}
---
+++ Runtime/HiraComponents/AI/LGOAP/Core/Domains/LayeredGoalOrientedActionPlannerDomain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace HiraEngine.Components.AI.LGOAP
+{
+    [CreateAssetMenu(menuName = "Hiralal/AI/LGOAP Domain", fileName = "New LGOAPDomain")]
+    [HiraCollectionCustomizer(1, MaxObjectCount = byte.MaxValue, Title = "Goals")]
+    [HiraCollectionCustomizer(2, MaxObjectCount = byte.MaxValue, Title = "Intermediate Goals")]
+    [HiraCollectionCustomizer(3, MaxObjectCount = byte.MaxValue, Title = "Actions")]
+    public class LayeredGoalOrientedActionPlannerDomain : HiraCollection<Goal, IntermediateGoal, Action>, IInitializable
+    {
+        public void Initialize<T>(ref T initParams)
+        {
+            
+        }
+
+        public void Shutdown()
+        {
+            
+        }
+    }
+}
