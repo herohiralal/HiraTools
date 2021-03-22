e74e34cfedac5b99c75f367da32fc6410b538538
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 22:30:19 +0530

    Created PlannerResult & GoalCalculatorJob.

---
+++ Runtime/HiraComponents/AI/LGOAP/Planner/GoalCalculatorJob.cs
@@ -0,0 +1,63 @@
+﻿using HiraEngine.Components.Blackboard;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+
+namespace HiraEngine.Components.AI.LGOAP.Internal
+{
+	[BurstCompile]
+	public unsafe struct GoalCalculatorJob : IJob
+	{
+		public GoalCalculatorJob(NativeArray<byte> blackboard, byte* insistenceCalculators, byte currentGoal, PlannerResult result)
+		{
+			_blackboard = blackboard;
+			_insistenceCalculators = insistenceCalculators;
+			_currentGoal = currentGoal;
+			Result = result;
+		}
+		
+		[ReadOnly] private readonly NativeArray<byte> _blackboard; // persistent
+		[ReadOnly] [NativeDisableUnsafePtrRestriction] private readonly byte* _insistenceCalculators; // persistent
+		[ReadOnly] private readonly byte _currentGoal; // managed
+		[WriteOnly] public PlannerResult Result; // reused
+	
+		public void Execute()
+		{
+			var blackboard = (byte*) _blackboard.GetUnsafeReadOnlyPtr();
+
+			var goal = byte.MaxValue;
+			var cachedInsistence = -1f;
+
+			var count = *(_insistenceCalculators + sizeof(ushort));
+			var stream = _insistenceCalculators + sizeof(ushort) + sizeof(byte);
+			for (byte i = 0; i < count; i++, stream += *(ushort*) stream)
+			{
+				var currentInsistence = MemoryBatchHelpers.ExecuteScoreCalculatorsBlock(blackboard, stream);
+
+				var foundBetterGoal = currentInsistence > cachedInsistence;
+
+				goal = foundBetterGoal ? i : goal;
+				cachedInsistence = foundBetterGoal ? currentInsistence : cachedInsistence;
+			}
+
+			if (goal == byte.MaxValue)
+			{
+				Result.ResultType = PlannerResultType.Failure;
+				Result.Count = 0;
+			}
+			else if (_currentGoal == goal)
+			{
+				Result.ResultType = PlannerResultType.Unchanged;
+				Result.Count = 1;
+				Result[0] = goal;
+			}
+			else
+			{
+				Result.ResultType = PlannerResultType.Success;
+				Result.Count = 1;
+				Result[0] = goal;
+			}
+		}
+	}
+}
---
+++ Runtime/HiraComponents/AI/LGOAP/Planner/PlannerResult.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace HiraEngine.Components.AI.LGOAP.Internal
+{
+	public enum PlannerResultType : byte
+	{
+		Uninitialized = 0,
+		Failure = 1,
+		Unchanged = 2,
+		Success = 3
+	}
+
+	public unsafe struct PlannerResult
+	{
+		public PlannerResult(byte bufferSize, Allocator allocator) =>
+			_container = new NativeArray<byte>(bufferSize + 2, allocator, NativeArrayOptions.UninitializedMemory)
+			{
+				[0] = 0,
+				[1] = (byte) PlannerResultType.Uninitialized
+			};
+
+		public void Dispose() => _container.Dispose();
+
+		private NativeArray<byte> _container;
+
+		public byte Count
+		{
+			get => _container[0];
+			set => _container[0] = value;
+		}
+
+		public PlannerResultType ResultType
+		{
+			get => (PlannerResultType) _container[1];
+			set => _container[1] = (byte) value;
+		}
+
+		public byte BufferSize => (byte) (_container.Length - 2);
+
+		public byte this[byte index]
+		{
+			get => _container[index + 2];
+			set => _container[index + 2] = value;
+		}
+
+		public byte* GetUnsafePtr() => 2 + (byte*) _container.GetUnsafePtr();
+
+		public byte* GetUnsafeReadOnlyPtr() => 2 + (byte*) _container.GetUnsafeReadOnlyPtr();
+
+		public void CopyTo(PlannerResult other) => _container.CopyTo(other._container);
+	}
+}
