ec208531eed8702db33058cc22dd0a9327dee239
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 10:21:30 +0530

    Created Executable/Service interfaces.

---
+++ Runtime/HiraComponents/AI/Common/Interfaces/IExecutable.cs
@@ -0,0 +1,15 @@
+﻿namespace HiraEngine.Components.AI
+{
+    public enum ExecutionStatus
+    {
+        InProgress, Succeeded, Failed
+    }
+    
+    public interface IExecutable
+    {
+        void OnExecutionStart();
+        ExecutionStatus Execute();
+        void OnExecutionFinish();
+        void OnExecutionAbort();
+    }
+}
