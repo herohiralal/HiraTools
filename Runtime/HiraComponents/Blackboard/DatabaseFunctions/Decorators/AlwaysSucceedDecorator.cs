34d8e01dfb90d04399eb6e4ab92eedca1719b96a
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-22 16:17:57 +0530

    Added always succeed decorator and base score calculator

---
+++ Runtime/HiraComponents/Blackboard/DatabaseFunctions/Decorators/AlwaysSucceedDecorator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.Burst;
+using UnityEngine;
+
+namespace HiraEngine.Components.Blackboard.Internal
+{
+    [Serializable, BurstCompile, HiraBlackboardDecorator]
+    public unsafe class AlwaysSucceedDecorator : ScriptableObject, IBlackboardDecorator
+    {
+        private static FunctionPointer<DecoratorDelegate> _function;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void CompileFunctions()
+        {
+            _function = BurstCompiler.CompileFunctionPointer<DecoratorDelegate>(FunctionImpl);
+        }
+
+        public virtual byte MemorySize => 0;
+
+        public virtual void AppendMemory(byte* memory)
+        {
+        }
+
+        [BurstCompile, AOT.MonoPInvokeCallback(typeof(DecoratorDelegate))]
+        private static bool FunctionImpl(byte* blackboard, byte* memory) => true;
+
+        public FunctionPointer<DecoratorDelegate> Function => _function;
+
+        public override string ToString() => "Always Succeed";
+        private void OnValidate() => name = ToString();
+    }
+}
---
+++ Runtime/HiraComponents/Blackboard/DatabaseFunctions/ScoreCalculators/BaseScoreCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.Burst;
+using UnityEngine;
+
+namespace HiraEngine.Components.Blackboard.Internal
+{
+    [Serializable, BurstCompile, HiraBlackboardDecorator]
+    public class BaseScoreCalculator : AlwaysSucceedDecorator, IBlackboardScoreCalculator
+    {
+        [SerializeField] private float score = 0f;
+
+        public float Score => score;
+        public override byte MemorySize => (byte) (sizeof(float) + base.MemorySize);
+
+        public override unsafe void AppendMemory(byte* stream)
+        {
+            *(float*) stream = Score;
+            base.AppendMemory(stream + sizeof(float));
+        }
+
+        public override string ToString() => $"Base ({score:+0;-#})";
+    }
+}
