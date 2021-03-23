230d491bbf29d63a40da2ceb2f20aa109deb8da6
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-23 17:37:45 +0530

    Fixed: wrong attribute in hiracollectioncustomizer in Goal.

--- Runtime/HiraComponents/AI/LGOAP/Core/Primitives/Goal.cs
+++ Runtime/HiraComponents/AI/LGOAP/Core/Primitives/Goal.cs
@@ -6,7 +6,7 @@ namespace HiraEngine.Components.AI.LGOAP
 {
     [BurstCompile]
     [HiraCollectionCustomizer(1, Title = "Insistence-Calculators", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardScoreCalculatorAttribute)})]
-    [HiraCollectionCustomizer(2, Title = "Targets", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(IBlackboardDecorator)})]
+    [HiraCollectionCustomizer(2, Title = "Targets", MaxObjectCount = byte.MaxValue, RequiredAttributes = new[] {typeof(HiraBlackboardDecoratorAttribute)})]
     public sealed class Goal : HiraCollection<IBlackboardScoreCalculator, IBlackboardDecorator>
     {
     }
