9705c919421fb2f90023c93a53f1485f7b3d1b53
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-03-26 14:50:40 +0530

    Created GenericPool because I'll be damned if I create a new executable every few frames.

---
+++ Runtime/HiraPoolTool/Core/GenericPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEngine
+{
+    public interface IPoolable
+    {
+        void OnRetrieve();
+        void OnReturn();
+    }
+    
+    public static class GenericPool<T> where T : IPoolable, new()
+    {
+        private static readonly List<T> pool = new List<T>();
+        
+        public static T Retrieve()
+        {
+            if (pool.Count == 0)
+                pool.Add(new T());
+
+            var last = pool.Count - 1;
+            var output = pool[last];
+            pool.RemoveAt(last);
+            output.OnRetrieve();
+            return output;
+        }
+
+        public static void Return(T target)
+        {
+            target.OnReturn();
+
+            pool.Add(target);
+        }
+    }
+}
