1688523300badac4c93598271931eac2eb6320bc
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-04-01 17:06:15 +0530

    Created FirstUpdate behaviour.

---
+++ Runtime/HiraFunctionLibraries/FirstUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine
+{
+    [HiraManager]
+    [AddComponentMenu("")]
+    [DefaultExecutionOrder(-10000)]
+    public class FirstUpdate : MonoBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void ResetExecutionQueue() => execution_queue.Clear();
+
+        private static readonly Queue<Action> execution_queue = new Queue<Action>();
+
+        private void Update()
+        {
+            while(execution_queue.Count>0)
+                execution_queue.Dequeue().Invoke();
+        }
+
+        public static void Catch(Action action) => execution_queue.Enqueue(action);
+    }
+}
